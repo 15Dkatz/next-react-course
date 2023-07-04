e91dcb064264fb6a1ffe2234962666d8d3d2db89
David Katz <15Dkatz@shcp.edu>
2023-07-04 00:09:48 -0700

    Style with Tailwind CSS | Styling

--- app/layout.js
+++ app/layout.js
@@ -13,22 +13,42 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div>
-          {
-            GRID_DATA_ITEMS.map((gridDataItem) => {
-              const { id, attributes } = gridDataItem;
+      <body className={`${inter.className} flex justify-center items-center min-h-screen`}>
+        <div className="w-[1000px] flex flex-col items-center pt-4 pb-4">
+          <div className="w-full bg-slate-100 flex flex-col items-center pb-10 pt-10">
+            <div className="text-2xl mb-5 underline decoration-3 decoration-blue-400">
+              DAVID KANDO MUSIC
+            </div>
+            <div className="grid grid-cols-4 gap-1">
+              {
+                GRID_DATA_ITEMS.map((gridDataItem) => {
+                  const { id, attributes } = gridDataItem;
 
-              return (
-                <div key={id}>
-                  <Link href={attributes.href}>{attributes.text}</Link>
-                </div>
-              )
-            })
-          }
+                  return (
+                    <Link key={id} href={attributes.href}>
+                      <div
+                        className={
+                          `${attributes.background} w-52 h-52
+                          flex items-end border-black border-[2px]`
+                        }
+                      >
+                        <div className={
+                          `bg-slate-200/75 w-full text-center mb-2
+                          hover:text-white hover:bg-slate-700/75`
+                          }
+                        >
+                          {attributes.text}
+                        </div>
+                      </div>
+                    </Link>
+                  )
+                })
+              }
+            </div>
+          </div>
+          <br />
+          {children}
         </div>
-        <br />
-        {children}
       </body>
     </html>
   )
